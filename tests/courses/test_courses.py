f87e8066c79514c93443cd8f5e0070ad6a514b08
Anna-QA-dev <anna.yefimenko.01@mail.ru>
2026-02-13 09:46:14 +0500

    8.2 Интеграция Allure в автотесты. Практическая работа с Allure аннотациями

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -1,12 +1,23 @@
+import allure
 import pytest
-
 from pages.courses.courses_list_page import CoursesListPage
 from pages.courses.create_course_page import CreateCoursePage
+from tools.allure.tags import AllureTag
+from tools.allure.epics import AllureEpic # Импортируем enum AllureEpic
+from tools.allure.features import AllureFeature # Импортируем enum AllureFeature
+from tools.allure.stories import AllureStory
+from allure_commons.types import Severity
 
 
 @pytest.mark.courses
 @pytest.mark.regression
+@allure.tag(AllureTag.REGRESSION, AllureTag.COURSES)
+@allure.epic(AllureEpic.LMS) # Добавили epic
+@allure.feature(AllureFeature.COURSES) # Добавили feature
+@allure.story(AllureStory.COURSES) # Добавили story
 class TestCourses:
+    @allure.title('Check displaying of empty courses list')
+    @allure.severity(Severity.NORMAL)
     def test_empty_courses_list(self, courses_list_page: CoursesListPage):
         courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
         courses_list_page.navbar.check_visible('username')
@@ -14,6 +25,8 @@ class TestCourses:
         courses_list_page.toolbar_view.check_visible()
         courses_list_page.check_visible_empty_view()
 
+    @allure.title('Create course')
+    @allure.severity(Severity.CRITICAL)
     def test_create_course(self, courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
         create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
         create_course_page.create_course_toolbar_view.check_visible()
@@ -42,6 +55,8 @@ class TestCourses:
         )
 
     class TestCourses:
+        @allure.title("Edit course")
+        @allure.severity(Severity.CRITICAL)
         def test_edit_course(
                 self,
                 courses_list_page: CoursesListPage,
